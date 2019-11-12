a6b0a7d0bbc8ea4f606172734b985f7e0bbf969c
yuyongyu <yuyongyu08@163.com>
2019-11-12 08:03:04 +0800

    modify module

--- 18-Module/2-VS-CommonJS/1-CommonJS/1-exports.js
+++ 18-Module/2-VS-CommonJS/1-CommonJS/1-exports.js
@@ -7,12 +7,18 @@ function getCounter(){
     return counter
 }
 
-console.log(module.exports === exports);
 
 module.exports = {
     counter,
     increase,
     getCounter
 }
+
+//知识点：module.exports和exports指向同一个对象，https://nodejs.org/docs/latest-v12.x/api/modules.html#modules_the_module_object
+// console.log(module.exports === exports);
+
+// exports.counter = counter;
+// exports.increase = increase;
+// exports.getCounter = getCounter;
 
 
